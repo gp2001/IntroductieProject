600c2b95da1a0d0d203fcb529d0d9a8768df81d5
Toan39 <71543094+Toan39@users.noreply.github.com>
2020-11-23 17:19:50 +0100

    SqlSetup1

--- TijdFunction1/TijdFunction1/Program.cs
+++ TijdFunction1/TijdFunction1/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 using System.Drawing;
 using System;
 
@@ -6,13 +8,44 @@ namespace TimeFunction1
 {
     class TimeMethod : Form
     {
-        //varabiales declarations
+        //varabiales declarations//
+        Button route;
 
         public TimeMethod()
         {
-            //declarations of panel
+            //declarations of panel//
+
+            //Route-knop//
+            route = new Button();
+            route.Text = "route";
+            route.Size = new Size(50, 20);
+            route.Location = new Point(600, 120);
+            this.Controls.Add(route);
+            route.Click += new EventHandler(route1_Click);
         }
+
+        public void route1_Click(object sender, EventArgs ea)
+        {
+            string connectionString;
+            SqlConnection connect;
+
+            connectionString = @"Data Source=DESKTOP-Q28TMM2;Initial Catalog=%databasename%;User ID=ta;Password=dat123";
+
+            connect = new SqlConnection(connectionString);
+            connect.Open();
+            
+            connect.Close();
+        }
+
         //void methods
+
+        //Void method for time
+
+        //draw event for time
+
+
+
+
     }
     class Timefunction1
     {
