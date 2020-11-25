5983dd439e43e5c13c1ae732bf545d5b15de7d41
gp2001 <74901857+gp2001@users.noreply.github.com>
2020-11-25 16:38:01 +0100

    Add files via upload

---
+++ Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Disneyland
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form5 map = new Form5();
+            map.Show();
+            this.Hide();
+        }
+    }
+}
